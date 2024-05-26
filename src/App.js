d5e07bd9df762131ab5873d54b2d6581acfc4e92
Rukshan Tharindu <97118309+Rukshan2000@users.noreply.github.com>
2024-05-26 08:20:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes from react-router-dom
+
+import Splash from './pages/SplashPage';
+import InvoicePage from './pages/InvoicePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes> {/* Use Routes instead of Router */}
+        <Route path="/" element={<Splash />} /> {/* Use element prop instead of component */}
+        <Route path="/invoicepage" element={<InvoicePage />} /> {/* Use element prop instead of component */}
+      </Routes>
+    </Router>
   );
 }
 
