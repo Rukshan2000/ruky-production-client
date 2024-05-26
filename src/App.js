08234fdc7e2a7de844032959e474f22e704a5bfa
Rukshan Tharindu <97118309+Rukshan2000@users.noreply.github.com>
2024-05-26 08:46:45 +0530

    password

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Im
 
 import Splash from './pages/SplashPage';
 import InvoicePage from './pages/InvoicePage';
+import PasswordPage from './pages/PasswordPage';
+
 
 function App() {
   return (
     <Router>
       <Routes> {/* Use Routes instead of Router */}
         <Route path="/" element={<Splash />} /> {/* Use element prop instead of component */}
+        <Route path="/passwordpage" element={<PasswordPage />} /> {/* Use element prop instead of component */}
         <Route path="/invoicepage" element={<InvoicePage />} /> {/* Use element prop instead of component */}
       </Routes>
     </Router>
