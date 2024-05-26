e2456b2c2f2c57406495d2c7d697430fe5cf68e6
Rukshan Tharindu <97118309+Rukshan2000@users.noreply.github.com>
2024-05-26 18:35:39 +0530

    pge breack

--- src/components/InvoiceTemplate.js
+++ src/components/InvoiceTemplate.js
@@ -35,6 +35,9 @@ function InvoiceTemplate({ data }) {
                             max-width: 210mm; /* A4 paper width */
                             margin: auto;
                         }
+                        .no-break-inside {
+                            page-break-inside: avoid;
+                        }
                     }
                 `}
             </style>
@@ -97,7 +100,7 @@ function InvoiceTemplate({ data }) {
                 </table>
                 <p className="text-red-500">{data.redNote}</p>
 
-                <div className="page-break">
+                <div className="no-break-inside">
                     <div className="flex justify-between">
                         <div className='mt-20'>
                             <p className="text-lg font-semibold">
@@ -116,20 +119,18 @@ function InvoiceTemplate({ data }) {
                             <p className="text-gray-700">K.V.R.U.Priyanath</p>
                         </div>
                     </div>
-               
+                </div>
 
-                <div className="flex items-center justify-between mt-20">
+                <div className="flex items-center justify-between mt-20 no-break-inside">
                     <img src={Qr} alt="Qr" className="h-16" />
                     <div>
                         <p className="mt-5 text-gray-700"><b>Contact</b><span className="ml-2">: +94 76 331 4026</span></p>
                         <p className="text-gray-700"><b>Email</b><span className="ml-6">: ruky.voice@gmail.com</span></p>
                     </div>
                 </div>
-                </div>
             </div>
             <button onClick={handlePrint} className="px-4 py-2 mt-4 text-white bg-blue-500 rounded">Download Invoice</button>
         </div>
-        
     );
 }
 
